ccb79b7c643a51192ae2f9bdfb7afc2847b27997
ismailofuz <sirojiddin.ismoilov7181@gmail.com>
2022-10-30 11:13:48 +0500

    chore: some changes and config email

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailSenderModule } from './mail-sender/mail-sender.module';
 
 @Module({
-  imports: [MailSenderModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MailSenderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/mail-sender/mail-sender.module.ts
+++ src/mail-sender/mail-sender.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { MailSenderService } from './mail-sender.service';
 import { MailSenderController } from './mail-sender.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('MAIL_HOST'),
+          secure: false,
+          auth: {
+            user: config.get('MAIL_USER'),
+            pass: config.get('MAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `"No reply" <${config.get('MAIL_FORM')}>`,
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [MailSenderController],
-  providers: [MailSenderService]
+  providers: [MailSenderService],
 })
 export class MailSenderModule {}
